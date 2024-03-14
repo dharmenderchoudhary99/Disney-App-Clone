d1cb6e166c738c692dcbd44e7c4f309907596da0
Dharmender Choudhary <dharmenderchoudhary022@gmail.com>
2024-03-14 15:39:02 +0530

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAMwi9E3eHm1BHOMcI4vCxqEhHSmhTroFY",
-  authDomain: "disneyplus-clone-ff72d.firebaseapp.com",
-  projectId: "disneyplus-clone-ff72d",
-  storageBucket: "disneyplus-clone-ff72d.appspot.com",
-  messagingSenderId: "759299041591",
-  appId: "1:759299041591:web:46cd7ef7ac565c0547d2b2",
-  measurementId: "G-5C59PZGVWW",
-};
+
 
 // const firebaseApp = firebase.initializeApp(firebaseConfig);
 // const db = firebaseApp.firestore();
